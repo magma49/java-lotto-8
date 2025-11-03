2a0ebdff3e731186ab18a71dff160584dce552d6
ndwPC <magma0409@gmail.com>
2025-11-03 16:42:08 +0900

    style: enum 사용하기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -134,15 +134,34 @@ public class Application {
     public static void printWinning(int[] winning, int purchase) {
         int prize = 0;
         System.out.print("3개 일치 (5,000원) - " + winning[0] + "개\n");
-        prize += winning[0] * 5;
+        prize += winning[0] * Rank.THREE.prize();
         System.out.print("4개 일치 (50,000원) - " + winning[1] + "개\n");
-        prize += winning[1] * 50;
+        prize += winning[1] * Rank.FOUR.prize();
         System.out.print("5개 일치 (1,500,000원) - " + winning[2] + "개\n");
-        prize += winning[2] * 1500;
+        prize += winning[2] * Rank.FIVE.prize();
         System.out.print("5개 일치, 보너스 볼 일치 (30,000,000원) - " + winning[3] + "개\n");
-        prize += winning[3] * 30000;
+        prize += winning[3] * Rank.BONUS.prize();
         System.out.print("6개 일치 (2,000,000,000원) - " + winning[4] + "개\n");
-        prize += winning[4] * 2000000;
+        prize += winning[4] * Rank.SIX.prize();
         System.out.print("총 수익률은 " + String.format("%.1f", (double) prize * 100 / purchase) + "%입니다.\n");
+    }
+
+    public enum Rank {
+        THREE(5),
+        FOUR(50),
+        FIVE(1500),
+        BONUS(30000),
+        SIX(2000000),
+        ;
+
+        private final int prize;
+
+        Rank(int prize) {
+            this.prize = prize;
+        }
+
+        public int prize() {
+            return prize;
+        }
     }
 }
